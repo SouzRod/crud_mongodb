3e6dab0f69a7d23d9370614256b44716d18426f0
Rodrigo Souza <40129317+SouzRod@users.noreply.github.com>
2022-06-15 14:19:30 -0300

    versionKey: false

--- src/model/product.js
+++ src/model/product.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    _id: { type: String },
+    id: { type: String },
     name: { type: String },
     brand: { type: String },
     model: { type: String },
     price: { type: Number },
 }, {
-    _id: false
+    _id: false,
+    versionKey: false,
 });
 
 module.exports = mongoose.model('Product', productSchema);
